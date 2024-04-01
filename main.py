55956c3b8adb051dc742a191e6e24b504e6bfd51
daniel1533b <165630135+daniel1533b@users.noreply.github.com>
2024-04-01 20:13:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
-st.set_page_config(page_title="meu site streamlit")
+st.set_page_config(page_title="Iphone Br")
 
 
-st.subheader("meu primeiro site com o streamlit")
-st.title("Dashboard de contratos")
+st.subheader("venha conferir nossa loja de iPhone com o melhor preço so mercado")
+st.title("entrega para todo o pais com frete livre")
 st.write("informaçoes sobre os contratos fechados pela hash&co ao longo de maio")
 st.write("quer aprender python")
