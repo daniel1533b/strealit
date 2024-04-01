c8693096abde5c91849518c07e72a53ac294b7c7
daniel1533b <165630135+daniel1533b@users.noreply.github.com>
2024-04-01 02:00:09 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="meu site streamlit")
+
+
+st.subheader("meu primeiro site com o streamlit")
+st.title("Dashboard de contratos")
+st.write("informaçoes sobre os contratos fechados pela hash&co ao longo de maio")
+st.write("quer aprender python")
