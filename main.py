002106d12112c5fadeef3243fc77eeac65f9b283
daniel1533b <165630135+daniel1533b@users.noreply.github.com>
2024-04-02 17:56:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ st.set_page_config(page_title="Iphone Br")
 
 
 st.subheader("iPhones BR")
-st.title("venha conferir nossa loja de iPhoneS com o melhor preço do mercado)
+st.title(")
 st.write("entrega para todo o BRASIL")
 st.write("")
