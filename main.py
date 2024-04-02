8e127aa77295a76dd9d244c7e268cf58e26c35fa
daniel1533b <165630135+daniel1533b@users.noreply.github.com>
2024-04-02 17:54:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(page_title="Iphone Br")
 
 
-st.subheader("venha conferir nossa loja de iPhone com o melhor preço so mercado")
-st.title("entrega para todo o pais com frete livre")
-st.write("informaçoes sobre os contratos fechados pela hash&co ao longo de maio")
-st.write("quer aprender python")
+st.subheader("iPhones BR")
+st.title("venha conferir nossa loja de iPhoneS com o melhor preço do mercado)
+st.write("entrega para todo o BRASIL")
+st.write("")
